57a53f3dd68b5df986ca26feafe556607114eabe
Reborn4797 <95199911+Reborn4749@users.noreply.github.com>
2026-04-26 00:53:58 +0700

    feat: add click event to button

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 let count = 0;
 const button = document.getElementById("feedBtn");
 const display = document.getElementById("treatCount");
+button.addEventListener("click", function() {
+    count = count + 1;
+    display.textContent = count;
+});
